50aa6526f7a8eb63406a89b1fed9d883ad2a46b9
roerES <erodriguezven@gmail.com>
2021-06-14 14:59:01 +0000

    Mi Primera App SAPUI5

---
+++ webapp/model/formatter.js
@@ -0,0 +1,39 @@
+sap.ui.define([
+	"sap/ui/core/ValueState"
+], function (ValueState) {
+	"use strict";
+	return {
+		formatterStatus: function (sValue) {
+			switch (sValue) {
+			case "C":
+				return ValueState.Success;
+			case "P":
+				return ValueState.Warning;
+			case "X":
+				return ValueState.Error;
+			default:
+				return ValueState.Information;
+			}
+		},
+		
+		statusIndicator: function (sStatus) {
+			var sIcon;
+
+			switch (sStatus) {
+			case "C":
+				sIcon = "completed";
+				break;
+			case "P":
+				sIcon = "in-process";
+				break;
+			case "X":
+				sIcon = "error";
+				break;
+			default:
+				sIcon = "inactive";
+			}
+
+			return "sap-icon://status-" + sIcon;
+		}
+	};
+});
---
+++ webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"roerdev/learn/FirtsUI5/test/unit/controller/App.controller"
+], function () {
+	"use strict";
+});
