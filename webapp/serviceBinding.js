73c4f1ec7bde933d2d0074c84a4cd64c6c9df667
roerES <erodriguezven@gmail.com>
2021-06-30 12:52:32 +0000

    j

--- webapp/serviceBinding.js
+++
@@ -1,5 +0,0 @@
-function initModel() {
-	var sUrl = "/sap/opu/odata/iwbep/GWSAMPLE_BASIC/";
-	var oModel = new sap.ui.model.odata.ODataModel(sUrl, true);
-	sap.ui.getCore().setModel(oModel);
-}
--- webapp/test/unit/AllTests.js
+++
@@ -1,5 +0,0 @@
-sap.ui.define([
-	"roerdev/learn/FirtsUI5/test/unit/controller/App.controller"
-], function () {
-	"use strict";
-});
